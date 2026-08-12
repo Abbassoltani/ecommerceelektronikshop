c5b28606db2dc504eb057aedd1e319ee2b2ad4ff
Abbassoltani <abbassoltani35@gmail.com>
2026-08-12 23:39:03 +0200

    all cdn

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 function Topbar() {
   return (
     <>
+    <div className="topbar py-2"></div>
+   
       
     </>
   )
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Topbar from '../components/Topbar'
 
 function MainLayouts() {
   return (
     <div>
-
+      <Topbar/>
     <Outlet/>
       
     </div>
